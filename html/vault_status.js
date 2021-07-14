eb2295dbae8e37578e833f54cc12c9d9d70afe5b
Alain Chiasson <alain@chaisson.org>
2021-07-14 11:01:38 -0400

    Trying just status

--- html/vault_status.js
+++ html/vault_status.js
@@ -6,30 +6,24 @@ class VaultStatus extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      vaultStatus: []
+      vaultStatus: {}
     };
   }
 
   render() {
+
+    const { vaultStatus } = this.state;  
+
     return (
     <div>
-      <center><h1>Contact List</h1></center>
-      {this.state.vaultStatus.map((contact) => (
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">{contact.name}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">{contact.email}</h6>
-                <p class="card-text">{contact.company.catchPhrase}</p>
-              </div>
-            </div>
-          ))}
+      <p>vault is initialized {vaultStatus.initialized} </p>
+      <p>vault version {vaultStatus.version} </p>
     </div>
     );
-  }
-
-
+  }  
+  
   componentDidMount() {
-    fetch('http://jsonplaceholder.typicode.com/users')
+    fetch('http://localhost:8080/v1/sys/health')
     .then(res => res.json())
     .then((data) => {
       this.setState({ VaultStatus: data })
