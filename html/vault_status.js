5f14b3fd81d6da75f07996038b51e4deb0244dd1
Alain Chiasson <alain@chaisson.org>
2021-07-15 06:52:48 -0400

    Added a vault status ui (simple)

--- html/vault_status.js
+++ html/vault_status.js
@@ -6,31 +6,51 @@ class VaultStatus extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      statusSet: false,
       vaultStatus: {}
     };
   }
 
   render() {
-
-    const { vaultStatus } = this.state;  
-
+    if (this.state.statusSet) {
+      return (
+        <div>
+        <p>status is set</p>
+        <p>version is {this.state.vaultStatus.version} </p>
+        </div>
+      );
+    }
     return (
-    <div>
-      <p>vault is initialized {vaultStatus.initialized} </p>
-      <p>vault version {vaultStatus.version} </p>
-    </div>
+      <div>
+        <p>status not set</p>
+      </div>
     );
   }  
-  
-  componentDidMount() {
+
+  getVaultStatus() {
+    console.log('getting state');
+    console.log('current state = ', this.state);
+
     fetch('http://localhost:8080/v1/sys/health')
     .then(res => res.json())
     .then((data) => {
-      this.setState({ VaultStatus: data })
+      this.setState({ 
+        vaultStatus: data
+      }), function () {
+        console.log(this.state.vaultStatus);
+      };
+    this.setState( {statusSet: true} );
+
     })
     .catch(console.log)
-    }
+  }
+  
+  componentDidMount() {
+    this.getVaultStatus();
+  }
+
 }
+
 // Find all DOM containers, and render Like buttons into them.
 ReactDOM.render(
   e(VaultStatus),
